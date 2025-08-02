25949f42814142ab996bc967c45d90e4e28f2af5
GAN <greer_00@naver.com>
2025-08-03 00:16:27 +0900

    index 화면
    메인 화면 기초틀

---
+++ Exhibition/webapp/js/main.js
@@ -0,0 +1,6 @@
+// 메뉴바
+function toggleSidebar() {
+    document.querySelector('.menu-trigger').classList.toggle('active');
+    document.querySelector('.sibebar').classList.toggle('open');
+    document.querySelector('.overlay').classList.toggle('open');
+}
